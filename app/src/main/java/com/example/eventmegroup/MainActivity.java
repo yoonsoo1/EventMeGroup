2873cdda44348e180218e9fc4e17ac6b55d00010
Yoonsoo Nam <yoonsoon@usc.edu>
2022-10-28 17:09:26 -0700

    Sign up with firebase done

--- app/src/main/java/com/example/eventmegroup/MainActivity.java
+++ app/src/main/java/com/example/eventmegroup/MainActivity.java
@@ -2,18 +2,32 @@ package com.example.eventmegroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView register;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if(currentUser == null) {
+            startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/eventmegroup/SignUpActivity.java
+++ app/src/main/java/com/example/eventmegroup/SignUpActivity.java
@@ -1,14 +1,20 @@
 package com.example.eventmegroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -32,8 +38,37 @@ public class SignUpActivity extends AppCompatActivity {
         signInText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+            }
+        });
 
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailSignUp.getText().toString();
+                String pass = passSignUp.getText().toString();
+
+                if(email.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Please Enter a Valid Email", Toast.LENGTH_SHORT).show();
+                }
+                else if(pass.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Please Enter a Valid Password", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(SignUpActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                // startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
             }
         });
     }
