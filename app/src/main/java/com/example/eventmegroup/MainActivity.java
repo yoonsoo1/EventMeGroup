050d260ffcb8fda8c7df797efec5208a4f581a89
Yoonsoo Nam <yoonsoon@usc.edu>
2022-10-31 17:11:16 -0700

    Sign out tested by clicking email in MainActivity

--- app/src/main/java/com/example/eventmegroup/MainActivity.java
+++ app/src/main/java/com/example/eventmegroup/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +27,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
-        finish();
-//        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-//        if(currentUser == null) {
-//            startActivity(new Intent(MainActivity.this, SignInActivity.class));
-//            finish();
-//        }
+//        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+//        finish();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if(currentUser == null) {
+            startActivity(new Intent(MainActivity.this, SignInActivity.class));
+            finish();
+        }
+        else {
+            TextView tv = findViewById(R.id.status);
+            tv.setText(currentUser.getEmail());
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    firebaseAuth.signOut();
+                    finish();
+                    startActivity(getIntent());
+                }
+            });
+        }
     }
 }
