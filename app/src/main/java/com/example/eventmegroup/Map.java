34c61a6f05273e6873b126c22c4f0ab91ac6915d
Oscar Hong <oscarhong25@gmail.com>
2022-11-07 20:02:43 -0800

    partial working map

--- app/src/main/java/com/example/eventmegroup/Map.java
+++ app/src/main/java/com/example/eventmegroup/Map.java
@@ -4,13 +4,13 @@ package com.example.eventmegroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,11 +19,22 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 //import com.example.eventmegroup.databinding.ActivityMapsBinding;
 
 public class Map extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.InfoWindowAdapter {
     private GoogleMap mMap;
-    Context context = null;
+    private FirebaseFirestore db;
+    private String uid;
+    private FirebaseAuth auth;
+    private Context context;
+
     LayoutInflater inflater;
 //    private ActivityMapsBinding binding;
 
@@ -41,14 +52,42 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback, Google
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").title("Melbourne"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        uid = auth.getCurrentUser().getUid();
 
-//        mMap.setInfoWindowAdapter(new Map(this));
+        db.collection("Events").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document : task.getResult()){
+                        String eventId = document.getId();
+//                        String name = (String) document.get("name");
+//                        String addy = (String) document.get("location");
+//                        String time = (String) document.get("time");
+//                        String cost = (String) document.get("cost");
+//                        String date = (String) document.get("date");
+//                        String sponsor = (String) document.get("sponsor");
+
+                        LatLng loc = new LatLng(Double.parseDouble((String) document.get("lat")), Double.parseDouble((String) document.get("long")));
+                        mMap.addMarker(new MarkerOptions().position(loc).title(eventId)
+                        );
+
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+                    }
+                }
+            }
+        });
+
         mMap.setInfoWindowAdapter(this);
     }
+
+//    @Override
+//    public boolean onMarkerClick(Marker marker) {
+//        mMap.setInfoWindowAdapter(new MarkerAdapter(getActivity(), marker
+//                .getTitle(), marker.getSnippet()));
+//        return false;
+//    }
 
     public Map(){
 //        this.context = context;
@@ -74,14 +113,41 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback, Google
     @Nullable
     @Override
     public View getInfoWindow(@NonNull Marker marker) {
+
         View v = getLayoutInflater().inflate(R.layout.map_box, null);
+        TextView mapName = (TextView) v.findViewById(R.id.e_name);
+
+//        mapName.setText("test string");
+//
+        String eventId = marker.getTitle();
+        db.collection("Events").document(eventId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+             //       for(QueryDocumentSnapshot document : task.getResult()){
+                        String name = (String) task.getResult().getString("name");
+                        String loc = (String) task.getResult().getString("location");
+                        String time = (String) task.getResult().getString("time");
+                        String cost = (String) task.getResult().getString("cost");
+                        String date = (String) task.getResult().getString("date");
+                        String sponsor = (String) task.getResult().getString("sponsor");
+
+                        System.out.println(name);
+
+
+                        mapName.setText("test");
+//                        mapName.setText(name);
+
+                }
+            }
+        });
+
         return v;
     }
 
     @Nullable
     @Override
     public View getInfoContents(@NonNull Marker marker) {
-
         return null;
     }
 }
