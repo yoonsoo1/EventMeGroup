4f3e8e0159f461f15043ce6a516b8c00198c4358
Yoonsoo Nam <yoonsoon@usc.edu>
2022-12-01 13:31:56 -0800

    Changed register page

--- app/src/main/java/com/example/eventmegroup/User.java
+++ app/src/main/java/com/example/eventmegroup/User.java
@@ -1,12 +1,18 @@
 package com.example.eventmegroup;
 
+import org.checkerframework.checker.units.qual.A;
+
 import java.util.ArrayList;
 
 public class User {
     private String uid;
     private ArrayList<String> registeredEvents;
+    private ArrayList<EventDate> eventDates;
+
     User(String uid) {
         this.uid = uid;
+        eventDates = new ArrayList<>();
+        registeredEvents = new ArrayList<>();
     }
 
     public void setRegisteredEvents(ArrayList<String> registeredEvents) {
@@ -15,6 +21,37 @@ public class User {
 
     public ArrayList<String> getRegisteredEvents() {
         return registeredEvents;
+    }
+
+    public void addEvent(String eventId, EventDate date) {
+        registeredEvents.add(eventId);
+        eventDates.add(date);
+    }
+
+    public int removeEvent(String eventId) {
+        int idx = -1;
+        for(int i = 0; i < registeredEvents.size(); i++) {
+            if(registeredEvents.get(i).equals(eventId)) {
+                idx = i;
+            }
+        }
+        if(idx == -1) {
+            return 1;
+        }
+        registeredEvents.remove(idx);
+        eventDates.remove(idx);
+        return 0;
+    }
+
+    public void setEventDates(ArrayList<String> eventDatesStr) {
+        String currDates;
+        for(int i = 0; i < eventDates.size(); i++) {
+            currDates = eventDatesStr.get(i);
+            // Array of eventDate is a concat string of date + time + duration
+            String[] eventD = currDates.split(" ");
+            EventDate ed = new EventDate(eventD[0], eventD[1], eventD[2]);
+            eventDates.add(ed);
+        }
     }
 
     public int registeredEvent(String event) {
@@ -30,4 +67,11 @@ public class User {
             }
         }
     }
+
+    public boolean eventConf(EventDate newEvent) {
+        // Check if the new event has any conflicts with the events in eventDates
+        return false;
+    }
 }
+
+
